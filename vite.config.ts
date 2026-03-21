cfa90eae0cdc202033cc0e83632cee5ce354803b
spaja86 <33865972+spaja86@users.noreply.github.com>
2026-03-21 10:36:43 +0100

    Create Vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'public',
+  },
+  server: {
+    port: 3000,
+  },
+});
